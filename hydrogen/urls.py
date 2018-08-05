11d33ffaa5af346126dcf5657657e6f54657cf2a
fuweifu-vtoo <vtoologo@163.com>
2018-08-05 15:07:57 +0800

    2018-8-5

--- hydrogen/urls.py
+++ hydrogen/urls.py
@@ -2,7 +2,7 @@
 # @Author: Jonathan fu
 # @Date:   2018-08-04 18:39:19
 # @Last Modified by:   jonathan fu
-# @Last Modified time: 2018-08-04 20:49:43
+# @Last Modified time: 2018-08-05 10:40:57
 from django.conf.urls import url
 
 from . import views
@@ -10,4 +10,6 @@ from . import views
 app_name = 'hydrogen'
 urlpatterns = [
     url(r'^$', views.hydrogen, name='hydrogen'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^about/$', views.about, name='about'),
 ]
--- hydrogen/views.py
+++ hydrogen/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def hydrogen(request):
     return render(request, 'hydrogen/index.html')
+    
+def contact(request):
+    return render(request, 'hydrogen/contact.html')
+
+def about(request):
+    return render(request, 'hydrogen/about.html')
