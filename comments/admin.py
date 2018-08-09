673f9d19975cc65d508f6d981e5eab90e875c2f3
fuweifu-vtoo <vtoologo@163.com>
2018-08-09 20:55:36 +0800

    ...

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Comment
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'text', 'created_time', 'post']
+
+admin.site.register(Comment,CommentAdmin)
--- comments/forms.py
+++ comments/forms.py
@@ -2,7 +2,7 @@
 # @Author: Jonathan fu
 # @Date:   2018-07-26 20:27:55
 # @Last Modified by:   jonathan fu
-# @Last Modified time: 2018-07-26 20:28:30
+# @Last Modified time: 2018-08-09 20:40:25
 
 from django import forms
 from .models import Comment
@@ -11,4 +11,4 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['name', 'email', 'url', 'text']
+        fields = ['name', 'email', 'text']
