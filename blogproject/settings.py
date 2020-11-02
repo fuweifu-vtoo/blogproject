85fa115c73aa76fcbee0de075f9b5be727041dfa
fuweifu-vtoo <vtoologo@163.com>
2020-11-02 17:02:14 +0800

    update

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jdxsw54mrn0_z1jl9xg=kbkph3u_9%)x_eh-jt^zgg)rba28u('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # 这个影响静态文件的加载，很迷惑
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.vtoo.pro', '39.105.59.116']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.vtoo.pro', '47.95.200.146']
 
 
 
